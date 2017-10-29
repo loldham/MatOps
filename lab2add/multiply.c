4968328c9167f55109a6b378defeadf5d57bc629
luke <luke.oldham.tj@gmail.com>
2017-10-28 21:48:59 -0700

    added build files

---
+++ lab2add/multiply.c
@@ -0,0 +1,29 @@
+#include "matops.h"
+
+//---multiply function taken from main.c
+
+void multiply (float** a, float** b, float** c, int n)
+{
+  int i,j,k;
+  for(i=0; i<n; i++)
+    for(j=0; j<n; j++)
+      c[i][j] = 0;
+
+  for(i=0; i<n; i++)
+    for(j=0; j<n; j++)
+      for(k=0; k<n; k++)
+        c[i][j] = a[i][k] * b[k][j];
+}
+
+//---print_matrix function taken from main.c
+
+void print_matrix(float** a, int n, char* mat_name)
+{
+  printf("Matrix %s:\n", mat_name);
+  for(int i=0; i<n; i++){
+    for(int j=0; j<n; j++)
+      printf("%g ", a[i][j]);
+    printf("\n");
+  }
+  printf("\n");
+}
